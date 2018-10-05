4a649b263db35b13da6cd4e204a4db39b25af8c9
K-Dog-TheQuest <korh0002@edu.easj.dk>
2018-10-05 13:32:56 +0200

    fixed sidebar

--- wp-content/themes/NewJystrup/index.php
+++ wp-content/themes/NewJystrup/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
-
+<?php get_sidebar(); ?>
 <!-- Other Content here -->
 <div id="posts" class="card">
-<?php get_sidebar(); ?>
+
     <?php if ( have_posts() ) : while ( have_posts() ) :   the_post(); ?>
         
         <h2>
